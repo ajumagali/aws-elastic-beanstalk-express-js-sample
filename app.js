6790cd36ba88e12f67188f0099d66458f66fd330
Asyl Jumagaliyev <ajum@Asyls-MacBook-Pro-2.local>
2021-08-04 15:30:44 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Manchester!'));
+app.get('/', (req, res) => res.send('This page was pushed to GitHub, then the it was built, reviewed manually and deployed to Elastic Beanstalk with the use of AWS CodePipeline and AWS CodeBuild'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
